5cca8c934db914d9a945f19c9d7049c2b09b93c2
piotrjurga <benny.profane37@gmail.com>
2022-03-02 09:13:16 +0100

    parsing input files into a matrix

--- main.cpp
+++ main.cpp
@@ -12,52 +12,62 @@ typedef int16_t  s16;
 typedef int32_t  s32;
 typedef int64_t  s64;
 
-struct Node {
-    std::vector<Node *> next;
-    std::vector<s32> cost;
+typedef float  f32;
+typedef double f64;
+
+struct GraphMatrix {
+    s32 *cells;
+    s32 dim;
 };
 
-struct Graph {
-    std::vector<Node> nodes;
-};
+void skip_lines(FILE *f, s32 count) {
+    char c = 0;
+    s32 lines_done = 0;
+    while (lines_done < count) {
+        s32 read_bytes = fread(&c, 1, 1, f);
+        if (read_bytes != 1) break;
+        if (c == '\n') lines_done++;
+    }
+}
 
-Graph parse_file(char *filepath) {
+GraphMatrix parse_file(const char *filepath) {
     FILE *file = fopen(filepath, "r");
     if (!file) return {};
 
     // skip first 6 lines
-    for (s32 i = 0; i < 6; i++) {
-        char line[80];
-        getline(line, sizeof(line), file);
-    }
+    skip_lines(file, 6);
 
     struct Pos2D { s32 x, y; };
     std::vector<Pos2D> positions;
     while (true) {
         s32 node_idx;
         Pos2D pos;
-        fscanf(" %d %d %d ", &node_idx, &pos.x, &pos.y);
+        s32 read_elems = fscanf(file, " %d %d %d ", &node_idx, &pos.x, &pos.y);
+        if (read_elems != 3) break;
         positions.push_back(pos);
     }
 
-    Graph result;
-    result.nodes.resize(positions.size());
-    for (s32 i = 0; i < result.nodes.size(); i++) {
-        auto& node = result.nodes[i];
-        node.next.resize(result.nodes.size());
-        node.cost.resize(result.nodes.size());
-        auto pos_a = positions[i];
-        for (s32 j = 0; j < result.nodes.size(); j++) {
-            node.next[j] = &result.nodes[j];
-            auto pos_b = positions[j];
-            s32 dx = pos_a.x - pos_b.x;
-            s32 dy = pos_a.y - pos_b.y;
+    GraphMatrix result;
+    result.dim = positions.size();
+    result.cells = (s32 *)malloc(sizeof(s32) * result.dim * result.dim);
+
+    for (s32 i = 0; i < result.dim; i++) {
+        Pos2D a = positions[i];
+        for (s32 j = 0; j < result.dim; j++) {
+            Pos2D b = positions[j];
+            s32 dx = a.x - b.x;
+            s32 dy = a.y - b.y;
             f32 delta = sqrtf(dx*dx + dy*dy);
-            node.cost[j] = (s32)(delta + 0.5f);
+            s32 cost = (s32)(delta + 0.5f);
+            result.cells[result.dim * i + j] = cost;
         }
     }
+
+    return result;
 }
 
 int main() {
     auto graph = parse_file("data/a280.tsp");
+
+    return 0;
 }
